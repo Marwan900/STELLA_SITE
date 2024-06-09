b67a4b1dcbc599aae1ed95585aa9ccee0faaa42c
Majidoy <abdelmajid.bouhamdane@gmailcom>
2024-06-09 21:45:23 +0200

    mise à jour du site

--- scripts.js
+++ scripts.js
@@ -1,39 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Function to fetch and display sensor data
-    function fetchSensorData() {
-        fetch('/ldr')
-            .then(response => response.text())
-            .then(data => {
-                const dataDisplay = document.getElementById("data-display");
-                if (data.includes("zone d'ombre")) {
-                    dataDisplay.classList.add("in-shadow");
-                } else {
-                    dataDisplay.classList.remove("in-shadow");
-                }
-                dataDisplay.innerHTML = `
-                    <p>${data}</p>
-                `;
-            })
-            .catch(error => console.error('Error fetching sensor data:', error));
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
 
-    // Function to fetch and display temperature data
-    function fetchTemperatureData() {
-        fetch('/temperature')
-            .then(response => response.text())
-            .then(data => {
-                const tempDisplay = document.getElementById("temp-display");
-                tempDisplay.innerHTML = `
-                    <p>${data}</p>
-                `;
-            })
-            .catch(error => console.error('Error fetching temperature data:', error));
-    }
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
 
-    fetchSensorData();
-    fetchTemperatureData();
+    sections.forEach(section => {
+        observer.observe(section);
+    });
 
-    // Refresh data every 10 seconds
-    setInterval(fetchSensorData, 10000);
-    setInterval(fetchTemperatureData, 10000);
+    // Simulation de la réception des données
+    setTimeout(() => {
+        document.getElementById('temp-display').textContent = "23°C";
+        document.getElementById('speed-display').textContent = "7.8 km/s / 0.01 g";
+        document.getElementById('altitude-display').textContent = "408 km";
+        document.getElementById('brightness-low-display').textContent = "Bas: 1.2 lx";
+        document.getElementById('brightness-high-display').textContent = "Haut: 1200 lx";
+    }, 2000);
 });
